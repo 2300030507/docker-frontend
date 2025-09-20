a2e9a9cb868df8427108309c7e41fa0bebb36979
2300030507 <163742216+2300030507@users.noreply.github.com>
2025-09-20 10:29:22 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-16-170-251-107.eu-north-1.compute.amazonaws.com
-/auth";
+const API_URL = "http://ec2-16-170-251-107.eu-north-1.compute.amazonaws.com/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
